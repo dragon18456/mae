e6abb21756d5e94b2ae63e3760390c58c6c388a9
Nicholas Lee <nicholas_lee@berkeley.edu>
2022-05-04 23:27:15 -0700

    Don't checkpoint every epoch

--- main_electra_finetune.py
+++ main_electra_finetune.py
@@ -306,7 +306,7 @@ def main(args):
             log_writer=log_writer,
             args=args
         )
-        if args.output_dir:
+        if args.output_dir and epoch > 0 and epoch % 20 == 0:
             misc.save_model(
                 args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
                 loss_scaler=loss_scaler, epoch=epoch)
